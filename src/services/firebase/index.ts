c76e0ed12d2400078ec68269a834a7c047f69bc1
Christian Leonardo <pleasefloyd73@gmail.com>
2022-12-01 17:19:43 -0300

    feat: firebase service config

---
+++ src/services/firebase/index.ts
@@ -0,0 +1,18 @@
+import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: `${import.meta.env.VITE_FIRESTORE_API_KEY}`,
+  authDomain: `${import.meta.env.VITE_FIRESTORE_AUTH_DOMAIN}`,
+  projectId: `${import.meta.env.VITE_FIRESTORE_PROJECT_ID}`,
+  storageBucket: `${import.meta.env.VITE_FIRESTORE_STORAGE_BUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_FIRESTORE_MESSAGING_SENDER_ID}`,
+  appId: `${import.meta.env.VITE_FIRESTORE_APP_ID}`,
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
